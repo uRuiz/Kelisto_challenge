0c8945e7cce909124194d4219d9c8287b723631a
Urko Ruiz <zaszur@gmail.com>
2018-11-11 23:51:20 +0100

    Challege de maquetacion terminado. Añadido minificacion de css

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 // importamos gulp
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
 
 // definimos tarea para compilar SASS
 gulp.task("default", ["compile-sass"], function(){
@@ -11,5 +14,9 @@ gulp.task("default", ["compile-sass"], function(){
 gulp.task("compile-sass", function(){
     gulp.src("./src/scss/style.scss") // cargamos el archivo
     .pipe(sass().on('error', sass.logError)) // compilamos el archivo SASS y controlamos errores
+    .pipe(postcss([
+        autoprefixer(), // autoprefija automáticamente el CSS
+        cssnano() // minifica el CSS
+    ]))
     .pipe(gulp.dest("./dist/css/")); // guardamos el archivo en dist/css
 });
